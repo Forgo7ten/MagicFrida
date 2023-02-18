f60c9a7a5fc14f010c428a1265817986a2aaae77
Forgo7ten <Forgo7ten2020@gmail.com>
2023-02-18 23:47:51 +0800

    patch some

--- envinit.sh
+++ envinit.sh
@@ -1,6 +1,7 @@
 echo "download dependency"
 sudo apt-get install -y build-essential curl git lib32stdc++-9-dev libc6-dev-i386 nodejs npm python3-dev python3-pip gcc-multilib g++-multilib
 sudo apt install -y unzip
+python3 -m pip install lief
 WORKSPACE=$(dirname $(pwd))
 if [ ! -d "$WORKSPACE/android-ndk-r22b" ]; then
   echo "ndk-r22b不存在"
@@ -11,12 +12,14 @@ if [ ! -d "$WORKSPACE/android-ndk-r22b" ]; then
   unzip ../android-ndk-r22b-linux-x86_64.zip -d ../
   echo "ndk解压成功. You can run 'rm -rf $WORKSPACE/android-ndk-r22b-linux-x86_64.zip' to delete the ndk cache."
 fi
-# sed -i 's/master/main/g' releng/frida-deps.vcxproj releng/frida.mk
 if [ ! -d "build" ]; then
   mkdir build
 fi
 python3 releng/generate-version-header.py "build/frida-version.h"
 echo "generate frida-version.h"
+cd frida-core
+git am ../Patchs/frida-core/*.patch && echo "Patch success!"
+cd ..
 echo "env init success!"
 echo "需手动设置环境变量"
 echo "=====>"
